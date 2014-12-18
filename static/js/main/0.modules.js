d428c03b95e6c689e6d07c7cdd07338301a4aed4
Jesse Yang <jesse.yang@booking.com>
2014-12-18 02:15:57 +0100

    initial commit

--- static/js/main/0.modules.js
+++
@@ -1,9 +0,0 @@
-<TMPL_INLINE _modules/require.js>
-<TMPL_INLINE _modules/fetch.js>
-
-<TMPL_JS_MODULE booking/index>
-<TMPL_JS_MODULE booking/exp>
-<TMPL_JS_MODULE booking/track>
-<TMPL_JS_MODULE booking/util>
-
-<TMPL_JS_MODULE ~jyang2/lightbox2>
