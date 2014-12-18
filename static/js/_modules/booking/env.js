bdba93f31b0974bd501d3073d6072d95864d1e81
Jesse Yang <jesse.yang@booking.com>
2014-12-18 18:23:13 +0100

    add missing js files

--- static/js/_modules/booking/env.js
+++ static/js/_modules/booking/env.js
@@ -1,14 +1,12 @@
 var env = {
-  debug: false,
-  expriments: {},
-  initializers: {},
+  debug: false
 };
 
 env.getVariant = function(name) {
   if (name in booking.jst) {
-    return booking.jst[name]
+    return booking.jst[name];
   }
-  return false
-}
+  return false;
+};
 
 module.exports = env;
